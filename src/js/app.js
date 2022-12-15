1fe938075288ef56a2755daa50969d0c7d7ecae9
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-16 00:10:37 +0200

    add js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import "../scss/app.scss";
+import * as R from 'ramda';
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -8,5 +9,11 @@ window.addEventListener("DOMContentLoaded", () => {
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
+  const plucked = R.pluck('class', arrayToPluck);
   const articles = document.querySelectorAll("article");
+
+  let count = 0;
+  for (let article of articles) {
+    article.classList.add(plucked[count++]);
+  }
 });
